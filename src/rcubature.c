d3b5ff87f01067c10594a8568118fec5ea231db4
Balasubramanian Narasimhan <naras@stat.stanford.edu>
2013-02-24 00:00:00 +0000

    version 1.1-2

--- src/rcubature.c
+++ src/rcubature.c
@@ -99,3 +99,13 @@ SEXP doCubature(SEXP sfDim, SEXP sf, SEXP sxLL, SEXP sxUL, SEXP smaxEval,
   UNPROTECT(5);
   return ans;
 }
+
+/**
+ * Register the native adapt_integrate functions so that they are C callable
+ * (courtesy of Simen Gaure)
+ */
+
+void R_init_cubature(DllInfo *info) {
+  R_RegisterCCallable("cubature", "adapt_integrate", (DL_FUNC) adapt_integrate);
+  R_RegisterCCallable("cubature", "adapt_integrate_v", (DL_FUNC) adapt_integrate_v);
+}
